d623e84ce8d99951ea78b02a061eee444790ace5
Nazax <gabrielon6689@gmail.com>
2025-03-20 20:40:17 -0300

    Update in end-point

--- src/main/java/com/nit/todolist/controllers/UserController.java
+++ src/main/java/com/nit/todolist/controllers/UserController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
